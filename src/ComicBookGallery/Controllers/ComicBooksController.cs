b2cd8959e7b20e2b36cc780186de60fa7d6aaeef
gganesha <siva_155@hotmail.com>
2019-01-22 00:55:34 -0500

    Updated Detail action method to use action result types w/ commentary on decision

--- src/ComicBookGallery/Controllers/ComicBooksController.cs
+++ src/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -11,11 +11,29 @@ namespace ComicBookGallery.Controllers
         //content result is a action result type by MVC.
         public ActionResult Detail()
         {
+            //to check if today's date
+
+            //RedirectResult() was not used since it cannot implicitly convert type
+            //System.Web.Mvc.RedirectResult to System.Web.Mvc.ContentResult
+            //Hovering over RedirectResult F12, shows it is an ActionResult
+            //Same is found when hovering over ContentResult, shows it is an ActionResult
+            //casting wouldn't solve the issue so changed the top to ActionResult
             if(DateTime.Today.DayOfWeek == DayOfWeek.Tuesday)
             {
                 return Redirect("/");
             }
             return Content("Hello from the comic books controller!");
-    }
+        }
+        /*
+         --> Using contentresult to display literal string (ComicBooks/Detail)
+        public ContentResult Detail()
+        {
+            return new ContentResult()
+            {
+                Content = "Hello from the comic books controller!";
+            }
+        }
+        */
+        
     }
 }
