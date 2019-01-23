a3f50e63f86905a46925447675efe286c000f771
gganesha <siva_155@hotmail.com>
2019-01-23 02:27:40 -0500

    Added comic book repository

--- src/ComicBookGallery/Controllers/ComicBooksController.cs
+++ src/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -11,23 +11,7 @@ namespace ComicBookGallery.Controllers
     {
         //content result is a action result type by MVC.
         public ActionResult Detail()
-        {
-            var comicBook = new ComicBook()
-            {
-                //object initializer syntax
-                SeriesTitle = "The Amazing Spider-Man",
-                IssueNumber = 700,
-                DescriptionHtml = "<p>Final issue! Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives... <strong>will Peter Parker?</strong></p>",
-                Artists = new Artists[]
-                {
-                    new Artists() {Name = "Dan Slott", Role = "Script"},
-                    new Artists() {Name = "Humberto Ramos", Role = "Pencils"},
-                    new Artists() {Name = "Victor Olazaba", Role = "Inks"},
-                    new Artists() {Name = "Edgar Delgado", Role = "Colors"},
-                    new Artists() {Name = "Chris Eliopoulos", Role = "Letters"},
-                }
-
-            };
+        {       
 
             //comicBook.SeriesTitle = "";
             //when switching from variables to Properties, should capitalize
